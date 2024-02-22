f1eb20e0eb7ea71e19a3dc70e98ee796f79ce2b3
Teddy Gallion <ted.j.gallion@gmail.com>
2024-02-21 19:23:51 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Main from "./Components/Main.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
+export default App
